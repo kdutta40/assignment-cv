30861e0914a4a6879e7fa33ae7751164e86f9773
Kanishka Dutta <duttakanishka40@gmail.com>
2025-04-04 17:50:53 +0530

    first commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,18 +5,18 @@ const Home = ({mode}) => {
     <div className={styles.container}>
         <div className={styles.contactBtns}>
           <ul className={mode ?styles.white :styles.black}>
-            <li><img src="/public/Facebook.png" alt="" srcset="" /></li>
-            <li><img src="/public/Instagram.png" alt="" /></li>
-            <li><img src="/public/Twitter.png" alt="" srcset="" /></li>
-            <li><img src="/public/GitHub.png" alt="" srcset="" /></li>
-            <li><img src="/public/Linkedin.png" alt="" srcset="" /></li>
-            <li><img src="/public/Telegram.png" alt="" srcset="" /></li>
-            <li><img src="/public/Koo.png" alt="" srcset="" /></li>
-            
+            <li><img src="src\assets\Facebook.png" alt="" srcset="" /></li>
+            <li><img src="src\assets\Instagram.png" alt="" /></li>
+            <li><img src="src/assets/Twitter.png" alt="" srcset="" /></li>
+            <li><img src="src\assets\GitHub.png" alt="" srcset="" /></li>
+            <li><img src="src\assets\LinkedIn.png" alt="" srcset="" /></li>
+            <li><img src="src\assets\Telegram.png" alt="" srcset="" /></li>
+            <li><img src="src\assets\Koo.png" alt="" srcset="" /></li>
+                                      
           </ul>
         </div>
         <div className={styles.info}>
-          <img src="public\Hello.png" alt="" />
+          <img src="src\assets\Hello.png" alt="" />
           <h1>I'm <span className={styles.line}><span className={styles.anish}>Anish</span></span> <img src="public/Wave.svg" alt="" /></h1> 
           <p>
             UI/UX Designer, Front-End Developer & Thinker <br />
@@ -28,10 +28,10 @@ const Home = ({mode}) => {
           </div>  
         </div>
         <div className={styles.pic}>
-          <img src="/public/image.png" alt="" />
+          <img src="src\assets\Image.png" alt="" />
         </div>
         <div className={styles.mail} >
-          <img src="/public/Email.png"  className={mode ?styles.w :styles.b} srcset="" />
+          <img src="src\assets\Email.png"  className={mode ?styles.w :styles.b} srcset="" />
         </div>
     </div>
   )
